c17f9b1c1b15422f4b3071e08bd139c738b21976
guy-jerome <aaronjamesroberts@gmail.com>
2024-03-28 16:14:24 -0400

    Finished the styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import { useState } from "preact/hooks";
-
 import "./app.css";
 
 export function App() {
+  const [mainImg, setMainImg] = useState("");
   const images: string[] = [
     "https://imgs.search.brave.com/bAdI7a7qnuuOIJphttt3DfbzLnCU7ZDMtnkejXq6UII/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9zdDIu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMDkz/OC81NDk5L2kvNDUw/L2RlcG9zaXRwaG90/b3NfNTQ5OTg2MTMt/c3RvY2stcGhvdG8t/Z2luZ2VyLWNhdC5q/cGc",
     "https://imgs.search.brave.com/YzG9FgYN5qLWsiYyJ4dUEvxav9e98dBH0loR8YPRA1E/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTc0/ODc3NTY1L3Bob3Rv/L3BvcnRyYWl0LW9m/LWEtYnJvd24tY2F0/LWFnYWluc3QtYS1n/cmF5LWJhY2tncm91/bmQuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPWlGcFFNSzlF/aTIzVm9XcExLa2Zh/TFVtdXN5Y3VaWllV/OWtWMjNzT2F6YzQ9",
@@ -10,10 +10,25 @@ export function App() {
     "https://imgs.search.brave.com/t6Nv0DwxoIACRxxtX2h7yt31ux5SCXHWgHVpoGh1diw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAyLzM2Lzk5LzIy/LzM2MF9GXzIzNjk5/MjI4M19zTk94Q1ZR/ZUZMZDVwZHFhS0do/OERSR01aeTdQNFhL/bS5qcGc",
   ];
   return (
-    <>
-      {images.map((image) => {
-        <image src={image}></image>;
-      })}
-    </>
+    <main>
+      <h4>Click on an Image!</h4>
+      <div id="cat-gal">
+        {images.map((image, index) => (
+          <img
+            className="cat-image"
+            src={image}
+            key={index}
+            alt="cat"
+            onClick={() => {
+              setMainImg(images[index]);
+            }}
+          />
+        ))}
+      </div>
+      <h3>
+        <b>Main Photo</b>
+      </h3>
+      {mainImg && <img className="cat-image-main" src={mainImg}></img>}
+    </main>
   );
 }
